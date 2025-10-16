29a24e589af56fbc990aea32f09396ea47f9eca8
NEOAPPS <asd22.info@gmail.com>
2025-10-16 22:03:51 +0300

    feat: git

---
+++ com/git-scm/git.lua
@@ -0,0 +1,35 @@
+pkg = {
+	name = "com.git-scm.git",
+	version = "2.51.0",
+	description = "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.",
+	maintainer = "NEOAPPS <neo@obsidianos.xyz>",
+	license = "GPL-2.0-only",
+	homepage = "https://git-scm.com/",
+	depends = {},
+	conflicts = {},
+	provides = { "git" },
+	files = {},
+}
+
+function pkg.source()
+	local version = pkg.version
+	local url = "https://www.kernel.org/pub/software/scm/git/git-" .. version .. ".tar.gz"
+	local source_dir = "git-" .. version
+
+	return function(hook)
+		hook("prepare")(function()
+			wget(url, "git.tar.gz")
+			sh("tar -zxf git.tar.gz")
+		end)
+
+		hook("build")(function()
+			sh("cd " .. source_dir .. " && make configure")
+			sh("cd " .. source_dir .. " && ./configure --prefix=/usr --with-openssl --with-curl --with-expat")
+			sh("cd " .. source_dir .. " && make")
+		end)
+
+		hook("install")(function()
+			sh("cd " .. source_dir .. " && make install DESTDIR=" .. shell_escape(ROOT))
+		end)
+	end
+end
