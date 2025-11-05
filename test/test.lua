7f50cb93954337cac2144027b8a752fab00f5bb5
NEOAPPS <asd22.info@gmail.com>
2025-11-05 11:57:52 +0300

    init(test.test)

---
+++ test/test.lua
@@ -0,0 +1,33 @@
+pkg = {
+	name = "test.test",
+	version = "NULL",
+	description = "Test Pkglet",
+	maintainer = "NEOAPPS <neo@obsidianos.xyz>",
+	license = "No idea",
+	homepage = "https://github.com/NULL-GNU-Linux",
+	depends = {},
+	conflicts = {},
+	provides = {},
+	files = {},
+}
+
+function pkg.binary()
+	return function(hook)
+		hook("install")(function() end)
+		hook("post_install")(function()
+			dump(OPTIONS)
+		end)
+	end
+end
+
+function pkg.uninstall()
+	return function(hook)
+		hook("pre_uninstall")(function() end)
+
+		hook("uninstall")(function() end)
+	end
+end
+
+function pkg.upgrade()
+	return function(from_version) end
+end
